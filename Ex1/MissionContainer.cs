d4a4ff7fd872dfcb60bb3966f0e0114ece58db8c
Almog Levi <almog.levi@live.biu.ac.il>
2019-03-14 16:33:15 +0200

    18:00

---
+++ Ex1/MissionContainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class FunctionsContainer
+    {
+
+        // members
+        private Dictionary<String, Func> funcsMap;     // the data structor.
+
+        // constructor
+        public FunctionsContainer()
+        {
+            funcsMap = new Dictionary<String, Func>();
+        }
+
+        // functions.
+        public Func this[String idx]
+        {
+            set
+            {
+                // allowing only one func in the deligat.
+                funcsMap[idx] = value;
+            }
+            get
+            {
+                if (!funcsMap.ContainsKey(idx))
+                {
+                    // defult func.
+                    funcsMap[idx] = var => var;
+                }
+                return funcsMap[idx];
+            }
+        }
+
+        public List<String> getAllMissions()
+        {
+            //get all names of missions.
+            var nameList = new List<String>(funcsMap.Keys);
+
+            return nameList;
+        }
+
+    }
+}
